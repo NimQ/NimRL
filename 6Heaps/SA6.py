de9e70d22a3f8b6da1176501d372d004f7788f52
NimQ <NimQ@users.noreply.github.com>
2015-05-21 13:28:59 +0200

    Create SA6.py

---
+++ 6Heaps/SA6.py
@@ -0,0 +1,37 @@
+import random as rnd
+import numpy as np
+
+class SA():
+    def __init__(self, board):
+        d = []
+        for i in range(0,board[0]+1):
+            for j in range(0,board[1]+1):
+                for k in range(0,board[2]+1):
+                    for l in range(0, board[3] + 1):
+                        for m in range(0, board[4] + 1):
+                            for n in range(0, board[5] + 1):
+                                d.append([i,j,k,l,m,n])
+
+        states = {i:d[i] for i in range(len(d))}
+        stringstates = [''.join(str(states[j])) for j in states]
+
+        stateindex = {stringstates[i]:i for i in range(len(stringstates))}
+
+        self.states = states
+        self.stateindex = stateindex
+
+        actions = []
+        for i in range(board[0]):
+            actions.append([0,i+1])
+        for i in range(board[1]):
+            actions.append([1,i+1])
+        for i in range(board[2]):
+            actions.append([2,i+1])
+        for i in range(board[3]):
+            actions.append([3,i+1])
+        for i in range(board[4]):
+            actions.append([4,i+1])
+        for i in range(board[5]):
+            actions.append([5,i+1])
+
+        self.actions = actions
